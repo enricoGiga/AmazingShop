171a748f1e6768ecf2a64791ae24398488f71d40
enricoGiga <enrico.gigante@gmail.co>
2024-09-29 20:32:56 +0200

    removed empty files

--- products_store/admin.py
+++ products_store/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from products_store.models import Product, Supplier
+from products_store.models import Product, Supplier, ProductImage
 
 admin.site.register(Product)
 admin.site.register(Supplier)
+admin.site.register(ProductImage)
